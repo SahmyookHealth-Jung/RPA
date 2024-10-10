1aac7a24471f55c35eeea6cdfbc7679e5cd02657
SahmyookHealth-Jung <SahmyookHealth-Jung@users.noreply.github.com>
2024-10-10 16:18:52 +0900

    5주차 실습파일

---
+++ 5week/fapi1.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"hello": "hi"}
+
+@app.get("/item")
+async def read_item(item_id : int, name: str = None, age : int =0):
+    return {"item_id": item_id, "name" : name, "age" : age}
+
+@app.get("/user")
+async def read_user(name : str = None, studentcode:int =0, major:str=None):
+    return{"msg: " + major + name + studentcode}
